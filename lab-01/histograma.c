92697084fe83305da321b24b3e21fb8db455fa25
agustinr <agustinandresrivero@gmail.com>
2024-03-16 18:22:18 -0300

    Histograma Terminado

--- lab-01/histograma.c
+++ lab-01/histograma.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    // Agregar código aquí.
+#define TAMANIO_MAXIMO 20
 
-    // Termina la ejecución del programa.
+int main(int argc, char *argv[])
+{	
+	
+	int histograma[TAMANIO_MAXIMO] = {0};	
+	char c;
+	int tamanioPalabra = 0;
+	
+	while((c = getchar()) != EOF)
+	{
+		if(c != '\n' && c!= ' ' && c!= '\t')
+		{
+			tamanioPalabra ++;
+		}
+		else
+		{
+			histograma[tamanioPalabra] ++;
+			tamanioPalabra = 0;		
+		}
+		
+	}
+	//dibujar el histograma
+	int i;
+	int j;
+	for(i = 1; i<=TAMANIO_MAXIMO; i++)
+	{
+		printf("%d", i);
+		for(j = 0; j < histograma[i]; j++)
+		{
+			putchar('*');
+		}
+		putchar('\n');
+	}
+	
+
     exit(EXIT_SUCCESS);
 }
