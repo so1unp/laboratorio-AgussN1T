7aa91e55d5da31a81ec21f2e04303ef2cf38c561
agustinr <agustinandresrivero@gmail.com>
2024-04-04 18:03:54 -0300

    avanzando con laboratorio

--- lab-03/sig_sender.c
+++ lab-03/sig_sender.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
-int main(void)
-{
-    __pid_t pid = getpid();
 
-    printf("Mi PID es: %d\n", pid);
+int main(int argc, char *argv[]) {
+    
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s <signal_number> <pid>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-    exit(EXIT_SUCCESS);
+    int signal_number = atoi(argv[1]);
+    __pid_t pid = atoi(argv[2]);
+
+    if (kill(pid, signal_number) == -1) {
+        perror("Error al enviar la señal");
+        return EXIT_FAILURE;
+    }
+
+    printf("Se envió la señal %d al proceso con PID %d\n", signal_number, pid);
+
+    return EXIT_SUCCESS;
 }
