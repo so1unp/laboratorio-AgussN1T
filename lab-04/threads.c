94e58d9408c88d8936ab8363c365bbf941f4d7c0
agustinr <agustinandresrivero@gmail.com>
2024-04-30 19:02:38 -0300

    avanzando con lab 4

--- lab-04/threads.c
+++ lab-04/threads.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int tiempo;
+
+void* funcionHilos(void * s){
+    unsigned int sleepTime = (rand() % tiempo + 1);
+    
+    long i = (long) s; 
+
+    printf("Hilo %ld Voy a dormir: %d\n", i, sleepTime);
+    sleep(sleepTime);
+
+    pthread_exit((void *)(intptr_t) sleepTime);
+}
+
 
 int main(int argc, char* argv[])
 {
+    int cantidadHilos = atoi(argv[1]);
+    tiempo = atoi(argv[2]);
+    pthread_t thread_ids[cantidadHilos];
+
+    long i;
+    for(i=0;i<cantidadHilos;i++){
+        pthread_create(&thread_ids[i],NULL,funcionHilos,(void *)i);
+    }
+
+    void * value;
+    for(i=0;i<cantidadHilos;i++){
+        pthread_join(thread_ids[i], &value);
+        printf("Tiempo que durmio el hilo %ld: %ld\n", i, (long)value);
+    }
+
+
     exit(EXIT_SUCCESS);
 }
-
